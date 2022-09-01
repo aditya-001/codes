e58ee8817631e1c79f54d4a3243447387df4c4fa
aditya-001 <aditya-001>
2022-09-01 20:38:33 +0530

    project files added for first time

---
+++ code_spyder.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 02:00:25 2022
+
+@author: aditya1117
+"""
+
+
+
---
+++ sample.py
@@ -0,0 +1,6 @@
+print("Hi")
+name=input("Enter You name:")
+age=input("Enter You AGe in Years:")
+age=int(age)
+birthYear=2022-age
+print("Hi "+ str(name) + ", " + "Your birth year is " + str(birthYear))
